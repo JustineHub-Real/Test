afd4922f824c6bff17e62d45f3a30abebd89bb01
JustineHub-Real <amudia.justine29@gmail.com>
2025-06-20 01:45:20 +0800

    update Test script.lua

--- Test script.lua
+++ Test script.lua
@@ -1,9 +1,24 @@
 local Window = OrionLib:MakeWindow({Name = "Kaze Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
 local main = Window:MakeTab({
 	Name = "Tab 1",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
+
 local Section = main:AddSection({
-	Name = "Section"
+	Name = "Speed 5 plus"
+})
+
+Tabs.main:Button({
+    Title = "Button test",
+    Desc = "",
+    Callback = function()
+        local plr = game:GetService("Players").LocalPlayer
+        local char = plr.Character or plr.CharacterAdded:Wait()
+        local humanoid = char:FindFirstChildOfClass("Humanoid")
+        if humanoid then
+            humanoid.WalkSpeed = humanoid.WalkSpeed + 5
+        end
+    end
 })
